d20410b4066ad553040ba411ce682e86ad3cd885
Oscar <64058694+OscarVarelaDev@users.noreply.github.com>
2023-08-14 18:45:02 -0600

    Initial Commit

---
+++ ListasGridSwiftUI/ListasGridSwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  ListasGridSwiftUIApp.swift
+//  ListasGridSwiftUI
+//
+//  Created by Oscar Varela on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ListasGridSwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
